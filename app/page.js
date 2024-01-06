cdd2b7db26ba18fd1b91bd74befc0a3c45223dcb
Tridominous <jamesmou02@gmail.com>
2024-01-06 01:24:43 +0000

    Add the header logo and responsive menu

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 
 import HeroSection from '@/components/HeroSection';
-import { Container } from 'postcss';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className='container mx-auto px-6'>
+      <Navbar/>
+      <div className='container mt-24 mx-auto px-12 py-4'>
       <HeroSection/>
       </div>
     </main>
